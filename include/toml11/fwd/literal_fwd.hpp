64b70f3dd83b3a3713ca9e9e40799605b934b514
Ger Hobbelt <ger@hobbelt.com>
2026-02-08 19:10:25 +0100

    fixes for C++23: user-defined literals, i.e. `operator ""_suffix()`

--- include/toml11/fwd/literal_fwd.hpp
+++ include/toml11/fwd/literal_fwd.hpp
@@ -19,12 +19,12 @@ inline namespace literals
 inline namespace toml_literals
 {
 
-::toml::value operator"" _toml(const char* str, std::size_t len);
+::toml::value operator""_toml(const char* str, std::size_t len);
 
 #if defined(TOML11_HAS_CHAR8_T)
 // value of u8"" literal has been changed from char to char8_t and char8_t is
 // NOT compatible to char
-::toml::value operator"" _toml(const char8_t* str, std::size_t len);
+::toml::value operator""_toml(const char8_t* str, std::size_t len);
 #endif
 
 } // toml_literals
